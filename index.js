e555043c197ec473f2691cf80b50e4abb2c4ca99
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-06-12 16:44:58 -0400

    index.js file updated with the prompt questions, classes and functions to create a new file named logo.svg when user answers all the questions. Changed the values of every shape so it can fit properly with the text.

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const inquirer = require ('inquirer');
 const fs = require("fs");
 const {Triangle, Circle, Square} = require('./lib/shapes')
 
+class SvgLogo{
+    constructor(){
+        this.svgString = ""
+        this.shapeEl = ""
+    }
+    render(){
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> ${this.shapeEl}${this.svgString} </svg>`
+    };
+    setTextEl(color, text) {
+        this.svgString = `<text x="150" y="125" font-size="40" text-anchor="middle" fill="${color}">${text}</text>`;
+      };
+    setShapeEl(shape){
+        this.shapeEl = shape.render()
+    }
+}
+
+function createSvgFile(data) {
+    fs.writeFile('logo.svg', data, (err) => {
+      if (err) {
+        console.error('Error creating SVG file:', err);
+      } else {
+        console.log('Generated logo.svg');
+      }
+    });
+  }
+
 function promptUser() {
     return inquirer.prompt([
         {
@@ -15,7 +41,7 @@ function promptUser() {
             name:"textColor",
         },
         {
-            type: "input",
+            type: "list",
             message:"Choose what shape you would like for your logo",
             choices: ['Circle', 'Triangle', 'Square'],
             name: "shape",
@@ -25,10 +51,31 @@ function promptUser() {
             message:"What shape's color would you like for your logo?",
             name:"shapeColor",
         },
-
-        
-
     ])
 };
 
+promptUser().then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
 
+    const logo = new SvgLogo();
+    logo.setTextEl(textColor, text);
+
+  switch (shape) {
+    case 'Circle':
+      logo.setShapeEl(new Circle(shapeColor));
+      break;
+    case 'Square':
+      logo.setShapeEl(new Square(shapeColor));
+      break;
+    case 'Triangle':
+      logo.setShapeEl(new Triangle(shapeColor));
+      break;
+    default:
+      console.log('Invalid shape');
+      return;
+  }
+  const svgContent = logo.render();
+  createSvgFile(svgContent);
+}).catch((err) => {
+  console.error('Error:', err);
+});
